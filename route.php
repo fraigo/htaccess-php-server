ba915107ec2200aa5b42d26127f6ad42f16c1e13
Francisco Igor <franciscoigor@gmail.com>
2018-08-11 21:29:53 -0700

    Fixed absolute path

--- route.php
+++ route.php
@@ -1,6 +1,6 @@
 <?php
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-    if (!file_exists($path)){
+    if (!file_exists(".$path")){
       include "api.php";
     } else {
       return false;
