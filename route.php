0de4a29de27a2caaf0ef782263c3e00ecfb0560d
Francisco Igor <franciscoigor@gmail.com>
2018-08-11 20:40:55 -0700

    Update route.php

--- route.php
+++ route.php
@@ -1,8 +1,6 @@
 <?php
-    chdir("..");
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
     if (!file_exists($path)){
-      chdir("backend");
       include "api.php";
     } else {
       return false;
